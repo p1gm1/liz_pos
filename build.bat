f25ca9b948cc752c57cee3623764edfd4cb8b0be
Alberto Perdomo <60266237+p1gm1@users.noreply.github.com>
2025-12-08 10:59:04 -0500

    Update build.bat

--- build.bat
+++ build.bat
@@ -5,6 +5,6 @@ call .venv\Scripts\activate
 echo "Installing dependencies..."
 pip install -r requirements.txt
 echo "Building executable..."
-pyinstaller --name "liz-pos" --onefile --windowed --add-data "src;src" src/main.py
+streamlit run src/main.py
 echo "Build finished."
 pause
